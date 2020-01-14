982b1691051b169eab4b73dcd17d6555a2243827
DF2877 <54418320+DF2877@users.noreply.github.com>
2020-01-13 20:01:57 -0500

    Add files via upload

---
+++ Data collect.R
@@ -0,0 +1,12 @@
+library(httr)
+library(jsonlite)
+library(data.table)
+
+teamnum <- 2877
+year <- 2019
+
+eventlist <- GET(paste("https://www.thebluealliance.com/api/v3/events/",year,"?X-TBA-Auth-Key=DzzDoXPk1JshyNjKpjkdDP2RHaqXNVD44xksasNYSxJu5YSmWYkTWvzA9stCcqrB",sep=""))
+rawevents <- rawToChar(eventlist$content)
+raweventlist <- fromJSON(rawevents)
+
+raweventlist
