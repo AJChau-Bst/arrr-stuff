b31c4caf5e672d905b6e14b592c25102412f9a35
DF2877 <54418320+DF2877@users.noreply.github.com>
2020-01-16 20:01:04 -0500

    Cleaned empty events out of fulldata list
    Discovered that there are some empty events. Not sure what that's about. Next up is formatting the matracies (oh boy)

--- Data collect.R
+++ Data collect.R
@@ -2,20 +2,33 @@ library(httr)
 library(jsonlite)
 library(data.table)
 
+# User input here
 teamnum <- 2877
 year <- 2019
 
+# Grabbing list of worldwide events from TBA API
 eventlist <- GET(paste("https://www.thebluealliance.com/api/v3/events/",year,"?X-TBA-Auth-Key=DzzDoXPk1JshyNjKpjkdDP2RHaqXNVD44xksasNYSxJu5YSmWYkTWvzA9stCcqrB",sep=""))
 rawevents <- rawToChar(eventlist$content)
 fullevents <- fromJSON(rawevents)
 
 reducedevents = fullevents$key
 
+# Grabbing match data for every event. This step takes a while.
 fulldata = list()
+j=1
 for(i in reducedevents){
   rawdata <- GET(paste("https://www.thebluealliance.com/api/v3/event/",i,"/matches?X-TBA-Auth-Key=DzzDoXPk1JshyNjKpjkdDP2RHaqXNVD44xksasNYSxJu5YSmWYkTWvzA9stCcqrB",sep=""))
   rawdata2 <- rawToChar(rawdata$content)
-  fulldata <- fromJSON(rawdata2)
+  fulldata[[j]] <- fromJSON(rawdata2)
+  j=j+1
 }
 
-fulldata
+# fulldata <- fulldata2 # <- For testing purposes
+
+# Cleaning fulldata of empty events (not entirely sure why those are there)
+for(k in length(fulldata):1){
+  if(length(fulldata[[k]])==0){
+    fulldata[[k]] <- NULL
+  }
+  
+}
