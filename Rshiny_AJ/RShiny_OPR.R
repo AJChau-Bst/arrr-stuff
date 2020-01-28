a2a5ca43fe4a9d48c36efe7c436ac9ad0a46b205
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-01-27 20:55:29 -0500

    oof

--- Rshiny_AJ/RShiny_OPR.R
+++ Rshiny_AJ/RShiny_OPR.R
@@ -3,10 +3,17 @@ library(shiny)
 
   
 ui <- fluidPage(
-  numericInput("obs", "Team Number",10, min = 1, max = 100),
+  magic <- numericInput("obs", "Team Number",10, min = 1, max = 100),
   verbatimTextOutput("value"),
   )
   server <- function(input, output) {
+    csv <- read.csv(file='ccwm.csv', header = FALSE)
+    matchvalue <- match(V1, V2, nomatch = NA_integer_, incomparables = NULL)
+    
+    
+    
+    
     output$value <- renderText({ input$obs })
   }
   shinyApp(ui, server)
+  
