a70b5b5e724212a9f28aa530e4e81c72c9522dab
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-01-31 14:51:36 -0500

    oof?

--- Rshiny_AJ/RShiny_OPR.R
+++ Rshiny_AJ/RShiny_OPR.R
@@ -7,7 +7,7 @@ ui <- fluidPage(
   verbatimTextOutput("value"),
   )
   server <- function(input, output) {
-    csv <- read.csv(file='ccwm.csv', header = FALSE)
+    csv <- read.csv(file='ccwm.csv', header = TRUE)
     matchvalue <- match(V1, V2, nomatch = NA_integer_, incomparables = NULL)
     
     
