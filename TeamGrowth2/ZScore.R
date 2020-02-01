3c8c60f531ad014cbd3b186cacb57950386480c2
DF2877 <54418320+DF2877@users.noreply.github.com>
2020-02-01 13:22:08 -0500

    more additions to zscore

--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -12,8 +12,16 @@ for (year in years) {
   opr <- tempdata$OPR
   oprmean <- mean(opr)
   oprsd <- sd(opr)
+  dpr <- tempdata$DPR
+  dprmean <- mean(dpr)
+  dprsd <- sd(dpr)
+  ccwm <- tempdata$CCWM
+  ccwmmean <- mean(ccwm)
+  ccwmsd <- sd(ccwm)
   for (a in 1:length(opr)) {
     oprzscore <- append(oprzscore,((opr[[a]]-oprmean)/oprsd))
+    dprzscore <- append(dprzscore,((dpr[[a]]-dprmean)/dprsd))
+    ccwmzscore <- append(ccwmzscore,((ccwm[[a]]-ccwmmean)/ccwmsd))
   }
 }
 
