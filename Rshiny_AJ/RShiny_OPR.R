b82b3284eebdb619f716e80191f5f76af55f7ab5
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-14 20:02:26 -0500

    idrk

--- Rshiny_AJ/RShiny_OPR.R
+++ Rshiny_AJ/RShiny_OPR.R
@@ -1,19 +1,17 @@
-library(shiny)
+library(rggobi)
+g <- ggobi(mydata)
+# Install iplots
+install.packages("iplots",dep=TRUE)
 
-
-  
-ui <- fluidPage(
-  magic <- numericInput("obs", "Team Number",10, min = 1, max = 100),
-  verbatimTextOutput("value"),
-  )
-  server <- function(input, output) {
-    csv <- read.csv(file='ccwm.csv', header = TRUE)
-    matchvalue <- match(V1, V2, nomatch = NA_integer_, incomparables = NULL)
-    
-    
-    
-    
-    output$value <- renderText({ input$obs })
-  }
-  shinyApp(ui, server)
-  
+# Create some linked plots
+library(iplots)
+cyl.f <- factor(mtcars$cyl)
+gear.f <- factor(mtcars$factor)
+attach(mtcars)
+iplot(mpg, wt) # scatter plot
+# Interacting with a scatterplot
+attach(mydata)
+plot(x, y) # scatterplot
+identify(x, y, labels=row.names(mydata)) # identify points
+coords <- locator(type="l") # add lines
+coords # display list
