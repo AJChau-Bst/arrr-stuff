87481ae80b6010de08a22249d0d1b539109d8f4d
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-18 18:43:27 -0500

    Starting over ish

--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -2,11 +2,13 @@ library(httr)
 library(jsonlite)
 library(data.table)
 
-years <- 2007:2010
+years <- 2007:2008
 
 teams <- vector()
 for (i in years) {
-  j <- paste("x",i,sep="")
   tempdata <- read.csv(paste(as.character(i), ".csv", sep = ""))
-  teams <- append(teams, tempdata$j)
+  teams <- append(teams, tempdata$Team)
 }
+
+uniqueteams <- unique(teams)
+
