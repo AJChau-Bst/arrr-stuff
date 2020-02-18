49d62daa2815552cbea7e4257c169cd6b199771c
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-18 18:50:21 -0500

    just continuing

--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -6,9 +6,14 @@ years <- 2007:2008
 
 teams <- vector()
 for (i in years) {
-  tempdata <- read.csv(paste(as.character(i), ".csv", sep = ""))
-  teams <- append(teams, tempdata$Team)
+  data <- read.csv(paste(as.character(i), ".csv", sep = ""))
+  teams <- append(teams, data$Team)
 }
 
 uniqueteams <- unique(teams)
 
+for (j in uniqueteams) {
+  for (k in years) {
+    
+  }
+}
