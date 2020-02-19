b02d283118bd3f5878cfb2d17151ec74884ad1ba
DF2877 <54418320+DF2877@users.noreply.github.com>
2020-02-19 18:56:15 -0500

    Oh boy I forgot to commit
    That's a lot of additions...

--- TeamGrowth2/Growth.R
+++ TeamGrowth2/Growth.R
@@ -3,6 +3,14 @@ library(jsonlite)
 library(data.table)
 
 # User input here
-teamnum <- 2877
+teamnum <- 254
 
-tempdata <- read.csv(year,)
+i <- getwd()
+
+teamdata <- read.csv(paste(i,"/Teams/",teamnum,".csv",sep=""))
+
+plot(teamdata$Year, teamdata$OPR, type = "l", col = "red", main = paste("Team ", teamnum),xlab = "Year", ylab = "Standardized Scores", xaxt = "none", ylim = c(-2, 2.5), lwd = 2)
+lines(teamdata$Year, teamdata$DPR, col = "deepskyblue", lwd = 2, xaxt = "none")
+lines(teamdata$Year, teamdata$CCWM, col = "green", lwd = 2, xaxt = "none")
+axis(1, teamdata$Year)
+legend("topleft", c("OPR", "DPR", "CCWM"), col = c("red", "deepskyblue", "green"), pch = 15, text.col = "black", horiz = TRUE)
--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -2,7 +2,7 @@ library(httr)
 library(jsonlite)
 library(data.table)
 
-years <- 2007:2019
+years <- 2007:2008
 
 teams <- vector()
 for (i in years) {
@@ -13,7 +13,7 @@ for (i in years) {
 uniqueteams <- unique(teams)
 
 m <- getwd()
-
+n <- 1
 for (j in uniqueteams) {
   umatrix <- matrix(,nrow=0,ncol=4)
   for (k in years) {
@@ -31,7 +31,7 @@ for (j in uniqueteams) {
     for (l in 1:length(tempteams)) {
       if (tempteams[l]==paste("frc",j,sep="")) {
         oprzscore <- (opr[l]-oprmean)/oprsd
-        dprzscore <- (dpr[l]-dprmean)/dprsd
+        dprzscore <- -(dpr[l]-dprmean)/dprsd
         ccwmzscore <- (ccwm[l]-ccwmmean)/ccwmsd
         umatrix <- rbind(umatrix, c(k,oprzscore,dprzscore,ccwmzscore))
       }
@@ -39,4 +39,6 @@ for (j in uniqueteams) {
   }
   colnames(umatrix) <- c("Year", "OPR", "DPR", "CCWM")
   write.csv(umatrix,file=paste(m,"/Teams/",j,".csv",sep=""),row.names=FALSE)
+  print(paste(paste(floor(100*n/length(uniqueteams)),"%",sep=""),j,sep="     "))
+  n=n+1
 }
