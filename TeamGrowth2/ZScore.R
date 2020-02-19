d897fddcfc551566afe8aed647641ff552bb84cd
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-18 20:53:26 -0500

    yay?

--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -2,7 +2,7 @@ library(httr)
 library(jsonlite)
 library(data.table)
 
-years <- 2007:2008
+years <- 2007:2019
 
 teams <- vector()
 for (i in years) {
@@ -12,14 +12,22 @@ for (i in years) {
 
 uniqueteams <- unique(teams)
 
+m <- getwd()
+
 for (j in uniqueteams) {
+  umatrix <- matrix(,nrow=0,ncol=4)
   for (k in years) {
     tempdata <- read.csv(paste(as.character(k), ".csv", sep = ""))
     tempteams <- tempdata$Team
+    opr <- tempdata$OPR
+    dpr <- tempdata$DPR
+    ccwm <- tempdata$CCWM
     for (l in 1:length(tempteams)) {
       if (tempteams[l]==paste("frc",j,sep="")) {
-        print(j)
+        umatrix <- rbind(umatrix, c(k,opr[l],dpr[l],ccwm[l]))
       }
     }
   }
+  colnames(umatrix) <- c("Team", "OPR", "DPR", "CCWM")
+  write.csv(umatrix,file=paste(m,"/Teams/",j,".csv",sep=""),row.names=FALSE)
 }
