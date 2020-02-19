d2114bf74d3556c7e54ae8516770b76245f210f0
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-18 19:18:06 -0500

    getting there

--- TeamGrowth2/ZScore.R
+++ TeamGrowth2/ZScore.R
@@ -6,14 +6,20 @@ years <- 2007:2008
 
 teams <- vector()
 for (i in years) {
-  data <- read.csv(paste(as.character(i), ".csv", sep = ""))
-  teams <- append(teams, data$Team)
+  tempdata <- read.csv(paste(as.character(i), ".csv", sep = ""))
+  teams <- append(teams, tempdata$Team)
 }
 
 uniqueteams <- unique(teams)
 
 for (j in uniqueteams) {
   for (k in years) {
-    
+    tempdata <- read.csv(paste(as.character(k), ".csv", sep = ""))
+    tempteams <- tempdata$Team
+    for (l in 1:length(tempteams)) {
+      if (tempteams[l]==paste("frc",j,sep="")) {
+        print(j)
+      }
+    }
   }
 }
