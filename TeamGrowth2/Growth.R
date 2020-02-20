842625b22e506fc32acb133e75b6412075cac2b7
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-02-20 16:02:38 -0500

    Update Growth.R

--- TeamGrowth2/Growth.R
+++ TeamGrowth2/Growth.R
@@ -9,7 +9,7 @@ i <- getwd()
 
 teamdata <- read.csv(paste(i,"/Teams/",teamnum,".csv",sep=""))
 
-plot(teamdata$Year, teamdata$OPR, type = "l", col = "red", main = paste("Team ", teamnum),xlab = "Year", ylab = "Standardized Scores", xaxt = "none", ylim = c(-4, 5), lwd = 2)
+plot(teamdata$Year, teamdata$OPR, type = "l", col = "red", main = paste("Team ", teamnum),xlab = "Year", ylab = "Standardized Scores", xaxt = "none", ylim = c(-5, 6), lwd = 2)
 lines(teamdata$Year, teamdata$DPR, col = "deepskyblue", lwd = 2, xaxt = "none")
 lines(teamdata$Year, teamdata$CCWM, col = "green", lwd = 2, xaxt = "none")
 axis(1, teamdata$Year)
