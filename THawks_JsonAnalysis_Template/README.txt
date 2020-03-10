36841e077f2678e22c12785d1b93f63634a9e42a
LigerBots CSO <59853554+LigerBotsCSO@users.noreply.github.com>
2020-03-09 20:48:40 -0400

    Beginning work on dashboard

--- THawks_JsonAnalysis_Template/README.txt
+++ THawks_JsonAnalysis_Template/README.txt
@@ -1,6 +1,6 @@
 Copy the folder somewhere convinient on your computer. Please do not use the folder on our GitHub.
 
-Copy all individual match JSON files into the folder. Note that they are .txt documents
+Copy all individual match JSON files into the folder. Note that they are .txt documents. They should be numbered 1-n matches.
 
 Open the json_team_analysis.R file with RStudio.
 To open the file you must have R and RStudio installed.
--- TeamGrowth2/Dashboard App.R
+++ TeamGrowth2/Dashboard App.R
@@ -7,21 +7,40 @@ library(shinydashboard)
 ui <- dashboardPage(
   
   dashboardHeader(
-    
+    title = "Team Viewing"
   ),
   
   dashboardSidebar(
-    
+    sidebarMenu(
+      id = "tabs",
+      textOutput("teamnum"),
+      menuItem("Individual Team Analysis", icon = icon("chart-area"),
+               menuSubItem(numericInput("team", "Input Team Number", 2877)),
+               menuSubItem("Plot", icon = icon("chart-bar")),
+               menuSubItem("Regresion", icon = icon("chart-line"))),
+      menuItem("Placeholder", icon = icon("chart-pie"),
+               menuSubItem("Placeholder2", icon = icon("cloudscale"))),
+      textOutput("tab")
+    )
   ),
   
   dashboardBody(
-    
+    tabItems(
+      tabItem(tabName = "Plot",
+              fluidRow(
+                box(
+                  plotOutput("single")
+                )
+              )
+      )
+    )
   )
 )
 
 server <- function(input, output){
   
-  output$Test <- renderText({"Yay"})
+  output$teamnum <- renderText({input$team})
+  output$tab <- renderText({input$tabs})
   
 }
 
